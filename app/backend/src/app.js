a66b7b18a5c8d4138eb9e7ad3e6b879d9fe50222
Vinicius Gomes <viniplay0599@gmail.com>
2023-01-02 22:15:28 -0300

    feat: implements swagger documentation

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+const swagger = require('swagger-ui-express');
 const cors = require('cors');
-
+const swaggerDocument = require('./swagger/swagger.json');
 const nf = require('./routes/notasFiscais');
 
 const app = express();
@@ -9,6 +9,8 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument));
 
 app.use('/nf', nf);
 
